5318abb7d9d0199ec2d4ffbf4ef02a578fa31426
Braxton Lyddon <braxtonlyddon@Braxtons-Air.local>
2022-03-14 20:56:21 -0500

    Enhancement: playing around with bash functions

--- zsh/work-aliases.sh
+++ zsh/work-aliases.sh
@@ -1,2 +1,2 @@
 alias emberssl='ember s --ssl --ssl-key ~/.tls/imtapps-active.key --ssl-cert ~/.tls/imtapps-active.crt'
-lias mrssl="./manage.py dev runsslserver --key ~/.tls/imtapps-active.key --certificate ~/.tls/imtapps-active.crt"
+alias mrssl="./manage.py dev runsslserver --key ~/.tls/imtapps-active.key --certificate ~/.tls/imtapps-active.crt"
